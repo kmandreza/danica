8548c9f879c55a6a58da7c8fcc3bfeeba7917f96
Khara Muniz <kmandreza@gmail.com>
2012-03-18 17:06:58 -0700

    First Draft

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Danica::Application.config.secret_token = '49e8852c5335002899075b2ba400bc78527a1908851193c674a5f62f69fa66179d4cc890acfaeeb83b50892fa2ba3843285df01fd7a72e6ce2def68dd752dd13'
